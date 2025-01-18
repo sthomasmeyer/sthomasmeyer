bf0564d71ebc2403b509b1878a0bf40215343307
Seth Thomasmeyer <sthomasmeyer@gmail.com>
2025-01-17 18:01:24 -0600

    edited ali birthday content for testing

--- .github/date-management/bash-scripts/january/muhammad-ali-birthday.sh
+++ .github/date-management/bash-scripts/january/muhammad-ali-birthday.sh
@@ -4,12 +4,15 @@
 source .github/date-management/utilities/gen-notable-date-header.sh
 
 output_file="README.md"
-date="01-17"
+date="01-18"
+image_path=".github/date-management/images/muhammad-ali-underwater.jpg"
 
 # Generate the header
 gen_notable_date_header "$date" "$output_file"
 
 # Append custom content
 {
-    echo "Float like a butterfly, sting like a bee."
+    echo "![]($image_path)"
+    echo ""
+    echo "\"Float like a butterfly, sting like a bee; the hands can't hit what the eyes can't see.\" - Muhammad Ali"
 } >> "$output_file"
